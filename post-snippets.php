9631ecbd5ff9f057de8d2e69e2935770a5ba2c14
JeroenSormani <jeroen.sormani@gmail.com>
2017-07-28 17:37:56 +0200

    Code consistency changes for post-snippets.php

--- post-snippets.php
+++ post-snippets.php
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /** Load all of the necessary class files for the plugin */
-spl_autoload_register('PostSnippets::autoload');
+spl_autoload_register( 'PostSnippets::autoload' );
 
 /**
  * Init Singleton Class.
@@ -38,227 +38,217 @@ spl_autoload_register('PostSnippets::autoload');
  */
 class PostSnippets
 {
-    /** Holds the plugin instance */
-    private static $instance = false;
+	/** Holds the plugin instance */
+	private static $instance = false;
 
-    /** Define plugin constants */
-    const MIN_PHP_VERSION     = '5.3.0';
-    const MIN_WP_VERSION      = '3.3';
-    const SETTINGS            = 'post_snippets';
-    const OPTION_KEY          = 'post_snippets_options';
-    const USER_META_KEY       = 'post_snippets';
-    const FILE                = __FILE__;
+	/** Define plugin constants */
+	const MIN_PHP_VERSION     = '5.3.0';
+	const MIN_WP_VERSION      = '3.3';
+	const SETTINGS            = 'post_snippets';
+	const OPTION_KEY          = 'post_snippets_options';
+	const USER_META_KEY       = 'post_snippets';
+	const FILE                = __FILE__;
 
-    /**
-     * Singleton class
-     */
-    public static function getInstance()
-    {
-        if (!self::$instance) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
+	/**
+	 * Singleton class
+	 */
+	public static function getInstance() {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Initializes the plugin.
-     */
-    private function __construct()
-    {
-        if (!$this->testHost()) {
-            return;
-        }
+	/**
+	 * Initializes the plugin.
+	 */
+	private function __construct() {
+		if ( ! $this->testHost() ) {
+			return;
+		}
 
-        load_plugin_textdomain(
-            'post-snippets',
-            false,
-            dirname(plugin_basename(__FILE__)).'/lang/'
-        );
+		load_plugin_textdomain(
+			'post-snippets',
+			false,
+			dirname( plugin_basename( __FILE__ ) ) . '/lang/'
+		);
 
-        register_uninstall_hook(__FILE__, array(__CLASS__, 'uninstall'));
-        add_action('after_setup_theme', array(&$this, 'phpExecState'));
+		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall' ) );
+		add_action( 'after_setup_theme', array( &$this, 'phpExecState' ) );
 
-        new \PostSnippets\Admin;
-        new \PostSnippets\WPEditor;
-        new \PostSnippets\Shortcode;
-    }
+		new \PostSnippets\Admin;
+		new \PostSnippets\WPEditor;
+		new \PostSnippets\Shortcode;
+	}
 
-    /**
-     * PSR-0 compliant autoloader to load classes as needed.
-     *
-     * @param  string  $classname
-     * @return void
-     */
-    public static function autoload($className)
-    {
-        if (__CLASS__ !== mb_substr($className, 0, strlen(__CLASS__))) {
-            return;
-        }
+	/**
+	 * PSR-0 compliant autoloader to load classes as needed.
+	 *
+	 * @param  string $classname
+	 * @return void
+	 */
+	public static function autoload( $className ) {
+		if ( __CLASS__ !== mb_substr( $className, 0, strlen( __CLASS__ ) ) ) {
+			return;
+		}
 
-        $className = ltrim($className, '\\');
-        $fileName  = '';
-        $namespace = '';
-        if ($lastNsPos = strrpos($className, '\\')) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
-            $fileName .= DIRECTORY_SEPARATOR;
-        }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className);
+		$className = ltrim($className, '\\');
+		$fileName  = '';
+		$namespace = '';
+		if ($lastNsPos = strrpos($className, '\\')) {
+			$namespace = substr($className, 0, $lastNsPos);
+			$className = substr($className, $lastNsPos + 1);
+			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
+			$fileName .= DIRECTORY_SEPARATOR;
+		}
+		$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className);
 
-        require 'src'.DIRECTORY_SEPARATOR.$fileName.'.php';
-    }
+		require 'src'.DIRECTORY_SEPARATOR.$fileName.' . php';
+	}
 
-    /**
-     * Fired when the plugin is uninstalled.
-     */
-    public function uninstall()
-    {
-        // Delete all snippets
-        delete_option('post_snippets_options');
+	/**
+	 * Fired when the plugin is uninstalled.
+	 */
+	public function uninstall() {
+		// Delete all snippets
+		delete_option('post_snippets_options');
 
-        // Delete any per user settings
-        global $wpdb;
-        $wpdb->query(
-            "
-            DELETE FROM $wpdb->usermeta
-            WHERE meta_key = 'post_snippets'
-            "
-        );
-    }
+		// Delete any per user settings
+		global $wpdb;
+		$wpdb->query(
+			"
+			DELETE FROM $wpdb->usermeta
+			WHERE meta_key = 'post_snippets'
+			"
+		);
+	}
 
 
-    // -------------------------------------------------------------------------
-    // Helpers
-    // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	// Helpers
+	// -------------------------------------------------------------------------
 
-    /**
-     * Allow snippets to be retrieved directly from PHP.
-     *
-     * @since   Post Snippets 1.8.9.1
-     *
-     * @param  string  $name  The name of the snippet to retrieve
-     * @param  string|array  $variables  The variables to pass to the snippet,
-     *         formatted as a query string or an associative array.
-     * @return string  The Snippet
-     */
-    public static function getSnippet($name, $variables = '')
-    {
-        $snippets = get_option(self::OPTION_KEY, array());
-        for ($i = 0; $i < count($snippets); $i++) {
-            if ($snippets[$i]['title'] == $name) {
-                if (!is_array($variables)) {
-                    parse_str(htmlspecialchars_decode($variables), $variables);
-                }
+	/**
+	 * Allow snippets to be retrieved directly from PHP.
+	 *
+	 * @since   Post Snippets 1.8.9.1
+	 *
+	 * @param  string       $name      The name of the snippet to retrieve
+	 * @param  string|array $variables The variables to pass to the snippet,
+	 *         formatted as a query string or an associative array.
+	 * @return string                  The Snippet
+	 */
+	public static function getSnippet($name, $variables = '') {
+		$snippets = get_option(self::OPTION_KEY, array());
+		for ($i = 0; $i < count($snippets); $i++) {
+			if ($snippets[$i]['title'] == $name) {
+				if (!is_array($variables)) {
+					parse_str(htmlspecialchars_decode($variables), $variables);
+				}
 
-                $snippet = $snippets[$i]['snippet'];
-                $var_arr = explode(",", $snippets[$i]['vars']);
+				$snippet = $snippets[$i]['snippet'];
+				$var_arr = explode(",", $snippets[$i]['vars']);
 
-                if (!empty($var_arr[0])) {
-                    for ($j = 0; $j < count($var_arr); $j++) {
-                        $snippet = str_replace(
-                            "{".$var_arr[$j]."}",
-                            $variables[$var_arr[$j]],
-                            $snippet
-                        );
-                    }
-                }
-                break;
-            }
-        }
-        return do_shortcode($snippet);
-    }
+				if (!empty($var_arr[0])) {
+					for ($j = 0; $j < count($var_arr); $j++) {
+						$snippet = str_replace(
+							"{".$var_arr[$j]."}",
+							$variables[$var_arr[$j]],
+							$snippet
+						);
+					}
+				}
+				break;
+			}
+		}
+		return do_shortcode($snippet);
+	}
 
-    // -------------------------------------------------------------------------
-    // Environment Checks
-    // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	// Environment Checks
+	// -------------------------------------------------------------------------
 
-    /**
-     * Checks PHP and WordPress versions.
-     */
-    private function testHost()
-    {
-        // Check if PHP is too old
-        if (version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<')) {
-            // Display notice
-            add_action('admin_notices', array(&$this, 'phpVersionError'));
-            return false;
-        }
+	/**
+	 * Checks PHP and WordPress versions.
+	 */
+	private function testHost() {
+		// Check if PHP is too old
+		if (version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<')) {
+			// Display notice
+			add_action('admin_notices', array(&$this, 'phpVersionError'));
+			return false;
+		}
 
-        // Check if WordPress is too old
-        global $wp_version;
-        if (version_compare($wp_version, self::MIN_WP_VERSION, '<')) {
-            add_action('admin_notices', array(&$this, 'wpVersionError'));
-            return false;
-        }
-        return true;
-    }
+		// Check if WordPress is too old
+		global $wp_version;
+		if (version_compare($wp_version, self::MIN_WP_VERSION, '<')) {
+			add_action('admin_notices', array(&$this, 'wpVersionError'));
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * Displays a warning when installed on an old PHP version.
-     */
-    public function phpVersionError()
-    {
-        echo '<div class="error"><p><strong>';
-        printf(
-            'Error: %3$s requires PHP version %1$s or greater.<br/>'.
-            'Your installed PHP version: %2$s',
-            self::MIN_PHP_VERSION,
-            PHP_VERSION,
-            $this->getPluginName()
-        );
-        echo '</strong></p></div>';
-    }
+	/**
+	 * Displays a warning when installed on an old PHP version.
+	 */
+	public function phpVersionError() {
+		echo '<div class="error"><p><strong>';
+		printf(
+			'Error: %3$s requires PHP version %1$s or greater . <br/>'.
+			'Your installed PHP version: %2$s',
+			self::MIN_PHP_VERSION,
+			PHP_VERSION,
+			$this->getPluginName()
+		);
+		echo '</strong></p></div>';
+	}
 
-    /**
-     * Displays a warning when installed in an old Wordpress version.
-     */
-    public function wpVersionError()
-    {
-        echo '<div class="error"><p><strong>';
-        printf(
-            'Error: %2$s requires WordPress version %1$s or greater.',
-            self::MIN_WP_VERSION,
-            $this->getPluginName()
-        );
-        echo '</strong></p></div>';
-    }
+	/**
+	 * Displays a warning when installed in an old Wordpress version.
+	 */
+	public function wpVersionError() {
+		echo '<div class="error"><p><strong>';
+		printf(
+			'Error: %2$s requires WordPress version %1$s or greater . ',
+			self::MIN_WP_VERSION,
+			$this->getPluginName()
+		);
+		echo '</strong></p></div>';
+	}
 
-    /**
-     * Get the name of this plugin.
-     *
-     * @return string The plugin name.
-     */
-    private function getPluginName()
-    {
-        $data = get_plugin_data(self::FILE);
-        return $data['Name'];
-    }
+	/**
+	 * Get the name of this plugin.
+	 *
+	 * @return string The plugin name.
+	 */
+	private function getPluginName() {
+		$data = get_plugin_data(self::FILE);
+		return $data['Name'];
+	}
 
-    // -------------------------------------------------------------------------
-    // Deprecated methods
-    // -------------------------------------------------------------------------
-    /**
-     * Allow plugins to disable the PHP Code execution feature with a filter.
-     * Deprecated: Use the POST_SNIPPETS_DISABLE_PHP global constant to disable
-     * PHP instead.
-     *
-     * @see   http://wordpress.org/extend/plugins/post-snippets/faq/
-     * @since 2.1
-     * @deprecated 2.3
-     */
-    public function phpExecState()
-    {
-        $filter = apply_filters('post_snippets_php_execution_enabled', true);
-        if ($filter == false and !defined('POST_SNIPPETS_DISABLE_PHP')) {
-            _deprecated_function(
-                'post_snippets_php_execution_enabled',
-                '2.3',
-                'define(\'POST_SNIPPETS_DISABLE_PHP\', true);'
-            );
-            define('POST_SNIPPETS_DISABLE_PHP', true);
-        }
-    }
+	// -------------------------------------------------------------------------
+	// Deprecated methods
+	// -------------------------------------------------------------------------
+	/**
+	 * Allow plugins to disable the PHP Code execution feature with a filter.
+	 * Deprecated: Use the POST_SNIPPETS_DISABLE_PHP global constant to disable
+	 * PHP instead.
+	 *
+	 * @see   http://wordpress.org/extend/plugins/post-snippets/faq/
+	 * @since 2.1
+	 * @deprecated 2.3
+	 */
+	public function phpExecState() {
+		$filter = apply_filters('post_snippets_php_execution_enabled', true);
+		if ($filter == false and !defined('POST_SNIPPETS_DISABLE_PHP')) {
+			_deprecated_function(
+				'post_snippets_php_execution_enabled',
+				'2.3',
+				'define( \'POST_SNIPPETS_DISABLE_PHP\', true);'
+			);
+			define( 'POST_SNIPPETS_DISABLE_PHP', true );
+		}
+	}
 }
 
-add_action('plugins_loaded', array('PostSnippets', 'getInstance'));
+add_action( 'plugins_loaded', array( 'PostSnippets', 'getInstance' ) );
