d76646a9185a1df2042724bac6211634f2145426
JeroenSormani <jeroen.sormani@gmail.com>
2017-07-28 17:38:31 +0200

    Code consistency changes for views/help/sidebar.php

--- views/help/sidebar.php
+++ views/help/sidebar.php
@@ -1,15 +1,15 @@
 <p><strong>
-<?php _e('Related:', 'post-snippets'); ?>
+<?php _e( 'Related:', 'post-snippets' ); ?>
 </strong></p>
 
 <p><a href="http://wordpress.org/support/plugin/post-snippets" target="_blank"><?php
-_e('Support Forums', 'post-snippets');
+_e( 'Support Forums', 'post-snippets' );
 ?></a></p>
 
 <p><a href="https://github.com/artstorm/post-snippets" target="_blank"><?php
-_e('GitHub Repository', 'post-snippets');
+_e( 'GitHub Repository', 'post-snippets' );
 ?></a></p>
 
 <p><a href="https://code.bitbebop.com/donate/" target="_blank"><?php
-_e('Donate', 'post-snippets');
+_e( 'Donate', 'post-snippets' );
 ?></a></p>
